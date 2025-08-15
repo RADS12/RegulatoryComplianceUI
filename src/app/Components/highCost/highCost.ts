cb441cdfc428ae922f30e6a399eda2e11252eab8
Srividhya Radhakrishnan <srirads@yahoo.com>
2025-08-15 17:36:48 -0400

    HighCost UI based on test result

--- src/app/Components/highCost/highCost.ts
+++ src/app/Components/highCost/highCost.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DataReaderService } from '../../Services/data-reader.service';
 
 @Component({
   selector: 'high-cost',
   templateUrl: './highCost.html',
-  styleUrls: ['./highCost.css']
+  styleUrls: ['./highCost.css'],
+  imports: [CommonModule]
 })
 export class HighCost implements OnInit {
   highCostData: any;
